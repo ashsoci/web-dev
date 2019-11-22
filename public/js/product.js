a4bf18c22e0cdb4b133a71ece3e54b6dab8cca0f
macdon51 <macdon51@coventry.ac.uk>
2019-11-22 16:39:52 +0000

    Added mobile columns

--- public/js/product.js
+++
@@ -1,9 +0,0 @@
-$( ".prod" )
-  .mouseenter(function() {
-    //n += 1;
-    //$( this ).find( "span" ).text( "mouse enter x " + n );
-	alert("Entered");
-  })
-  .mouseleave(function() {
-    //$( this ).find( "span" ).text( "mouse leave" );
-  });
