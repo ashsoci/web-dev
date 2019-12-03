cd2af586d84899bf95c5342e0d2f7b886af43777
macdon51 <macdon51@coventry.ac.uk>
2019-12-03 20:21:25 +0000

    Added delete button functions

--- public/js/admin.js
+++ public/js/admin.js
@@ -115,6 +115,57 @@ function deleteTeamMember()
 	ajax.send(JSON.stringify(formData));
 }
 
+function deleteProduct()
+{
+	event.preventDefault();
+	var product = document.getElementById('product-form');
+
+	var formData = 
+	{
+		id: product.elements.namedItem('id').value,
+		title: product.elements.namedItem('name').value,
+		pathToImg: product.elements.namedItem('thumbnail-name').value,
+		productBanner: product.elements.namedItem('banner-name').value,
+		productSlide1: product.elements.namedItem('slide-1-name').value,
+		productSlide2: product.elements.namedItem('slide-2-name').value,
+		productSlide3: product.elements.namedItem('slide-3-name').value,
+		productSlide4: product.elements.namedItem('slide-4-name').value,
+		productSlide5: product.elements.namedItem('slide-5-name').value,
+		productHeader1: product.elements.namedItem('headline-1').value,
+		productHeader2: product.elements.namedItem('headline-2').value,
+		productHeader3: product.elements.namedItem('headline-3').value,
+		productFeature1: product.elements.namedItem('paragraph-1').value,
+		productFeature2: product.elements.namedItem('paragraph-2').value,
+		productFeature3: product.elements.namedItem('paragraph-3').value,
+		productRam: product.elements.namedItem('ram').value,
+		productHdd: product.elements.namedItem('hdd').value
+	}
+	
+	var ajax = new XMLHttpRequest();
+	
+	ajax.onreadystatechange = function() {
+		console.log(this);
+		console.log(this.status);
+		if (this.readyState == 4 && this.status == 201) {
+			var data = JSON.parse(this.responseText);
+			console.log("success");
+
+			var but = document.getElementById("productDeleteButton");
+			but.setAttribute("disabled", "true");
+			but.innerHTML = "Deleting...";
+			
+			setTimeout(function() 
+			{
+				window.location.href = "/admin/admin-products";
+			}, 5000);
+		}
+	};
+	
+	ajax.open('post', '/delete-product');
+	ajax.setRequestHeader("Content-Type", "application/json");
+	ajax.send(JSON.stringify(formData));
+}
+
 function submitProductForm()
 {
 	console.log("Submitting");
